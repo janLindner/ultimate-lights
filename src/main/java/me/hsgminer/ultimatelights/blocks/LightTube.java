7d42cf3ed5ce0823869f186810347b5a90988270
Jan Lindner <jan.lindner@online.de>
2020-10-23 18:29:21 +0200

    add light tube shapes

--- src/main/java/me/hsgminer/ultimatelights/blocks/LightTube.java
+++ src/main/java/me/hsgminer/ultimatelights/blocks/LightTube.java
@@ -23,6 +23,30 @@ public final class LightTube extends AbstractHorizontalFaceLightBlock {
     private static final VoxelShape FLOOR_BORDER_NORTH = makeCuboidShape(5, 0, 0, 11, 7, 1);
     private static final VoxelShape FLOOR_BORDER_SOUTH = makeCuboidShape(5, 0, 15, 11, 7, 16);
 
+    private static final VoxelShape CEILING_TUBE_EAST_WEST = makeCuboidShape(0, 10, 6, 16 , 14 ,10);
+    private static final VoxelShape CEILING_BORDER_WEST = makeCuboidShape(0, 9, 5, 1, 16, 11);
+    private static final VoxelShape CEILING_BORDER_EAST = makeCuboidShape(15, 9, 5, 16, 16, 11);
+    private static final VoxelShape CEILING_TUBE_NORTH_SOUTH = makeCuboidShape(6, 10, 0, 10 , 14 ,16);
+    private static final VoxelShape CEILING_BORDER_NORTH = makeCuboidShape(5, 9, 0, 11, 16, 1);
+    private static final VoxelShape CEILING_BORDER_SOUTH = makeCuboidShape(5, 9, 15, 11, 16, 16);
+
+    private static final VoxelShape WALL_EAST_TUBE = makeCuboidShape(2, 6, 0, 6, 10, 16);
+    private static final VoxelShape WALL_EAST_BORDER_0 = makeCuboidShape(0, 5, 0, 7, 11, 1);
+    private static final VoxelShape WALL_EAST_BORDER_1 = makeCuboidShape(0, 5, 15, 7, 11, 16);
+
+    private static final VoxelShape WALL_WEST_TUBE = makeCuboidShape(10, 6, 0, 14, 10, 16);
+    private static final VoxelShape WALL_WEST_BORDER_0 = makeCuboidShape(9, 5, 0, 16, 11, 1);
+    private static final VoxelShape WALL_WEST_BORDER_1 = makeCuboidShape(9, 5, 15, 16, 11, 16);
+
+    private static final VoxelShape WALL_SOUTH_TUBE = makeCuboidShape(0, 6, 2, 16, 10, 6);
+    private static final VoxelShape WALL_SOUTH_BORDER_0 = makeCuboidShape(15, 5, 0, 16, 11, 7);
+    private static final VoxelShape WALL_SOUTH_BORDER_1 = makeCuboidShape(0, 5, 0, 1, 11, 7);
+
+    private static final VoxelShape WALL_NORTH_TUBE = makeCuboidShape(0, 6, 10, 16, 10, 14);
+    private static final VoxelShape WALL_NORTH_BORDER_0 = makeCuboidShape(15, 5, 9, 16, 11, 16);
+    private static final VoxelShape WALL_NORTH_BORDER_1 = makeCuboidShape(0, 5, 9, 1, 11, 16);
+
+
     public LightTube(
         @NotNull final DyeColor color
     ) {
@@ -59,6 +83,48 @@ public final class LightTube extends AbstractHorizontalFaceLightBlock {
 
             if (!state.get(NORTH)) shape = VoxelShapes.or(shape, FLOOR_BORDER_NORTH);
             if (!state.get(SOUTH)) shape = VoxelShapes.or(shape, FLOOR_BORDER_SOUTH);
+        }
+
+        if (face == AttachFace.CEILING && (direction == Direction.NORTH || direction == Direction.SOUTH)) {
+            shape = VoxelShapes.or(shape, CEILING_TUBE_EAST_WEST);
+
+            if (!state.get(EAST)) shape = VoxelShapes.or(shape, CEILING_BORDER_EAST);
+            if (!state.get(WEST)) shape = VoxelShapes.or(shape, CEILING_BORDER_WEST);
+        }
+
+        if (face == AttachFace.CEILING && (direction == Direction.WEST || direction == Direction.EAST)) {
+            shape = VoxelShapes.or(shape, CEILING_TUBE_NORTH_SOUTH);
+
+            if (!state.get(NORTH)) shape = VoxelShapes.or(shape, CEILING_BORDER_NORTH);
+            if (!state.get(SOUTH)) shape = VoxelShapes.or(shape, CEILING_BORDER_SOUTH);
+        }
+
+        if (face == AttachFace.WALL && direction == Direction.NORTH) {
+            shape = VoxelShapes.or(shape, WALL_NORTH_TUBE);
+
+            if (!state.get(EAST)) shape = VoxelShapes.or(shape, WALL_NORTH_BORDER_0);
+            if (!state.get(WEST)) shape = VoxelShapes.or(shape, WALL_NORTH_BORDER_1);
+        }
+
+        if (face == AttachFace.WALL && direction == Direction.SOUTH) {
+            shape = VoxelShapes.or(shape, WALL_SOUTH_TUBE);
+
+            if (!state.get(EAST)) shape = VoxelShapes.or(shape, WALL_SOUTH_BORDER_0);
+            if (!state.get(WEST)) shape = VoxelShapes.or(shape, WALL_SOUTH_BORDER_1);
+        }
+
+        if (face == AttachFace.WALL && direction == Direction.EAST) {
+            shape = VoxelShapes.or(shape, WALL_EAST_TUBE);
+
+            if (!state.get(NORTH)) shape = VoxelShapes.or(shape, WALL_EAST_BORDER_0);
+            if (!state.get(SOUTH)) shape = VoxelShapes.or(shape, WALL_EAST_BORDER_1);
+        }
+
+        if (face == AttachFace.WALL && direction == Direction.WEST) {
+            shape = VoxelShapes.or(shape, WALL_WEST_TUBE);
+
+            if (!state.get(NORTH)) shape = VoxelShapes.or(shape, WALL_WEST_BORDER_0);
+            if (!state.get(SOUTH)) shape = VoxelShapes.or(shape, WALL_WEST_BORDER_1);
         }
 
         return shape;
